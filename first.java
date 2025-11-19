3478666d5291b3b73c2743e1ae481a20d0d84c46
coffee-lovers-code <arseniicalinin@yandex.ru>
2025-11-19 22:31:03 +0300

    Add first.java

---
+++ first.java
@@ -0,0 +1,15 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = System.out;
+    public static void main(String[] args) {
+        int first = in.nextInt();
+        int count = in.nextInt();
+        for(int i = 0; i < count; i++){
+            if(i % 2 == 0) first += 4;
+            else first *= 3;
+            out.print(first + " ");
+        }
+    }
+}
